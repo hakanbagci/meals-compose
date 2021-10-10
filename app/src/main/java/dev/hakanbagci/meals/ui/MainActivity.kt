7acbcbcefd90ae27131b059f36ebc1ac92b237d8
hakanbagci <hbagci@gmail.com>
2021-10-10 15:31:58 +0200

    Setup hilt

--- app/src/main/java/dev/hakanbagci/meals/ui/MainActivity.kt
+++ app/src/main/java/dev/hakanbagci/meals/ui/MainActivity.kt
@@ -1,4 +1,4 @@
-package dev.hakanbagci.meals
+package dev.hakanbagci.meals.ui
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -8,8 +8,10 @@ import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import dagger.hilt.android.AndroidEntryPoint
 import dev.hakanbagci.meals.ui.theme.MealsTheme
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
